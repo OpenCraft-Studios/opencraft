8771431e352d8b90f79c9bf85184e1bdbf68e1b6
SrFerraaaari <cirod307@gmail.com>
2025-02-28 15:39:04 +0100

    🤡 `src/main.c`: GLFW "getting started"
    This is going to faiiil but i dont caareee!
    
    \__😉__/

--- src/main.c
+++ src/main.c
@@ -1,7 +1,43 @@
 # include <stdio.h>
+# include <GLFW/glfw3.h>
 
-int main(void)
+static GLFWwindow* window;
+
+int main( void )
 {
-    printf("Hello world!\n");
+    if (!glfwInit())
+    {
+        printf("[ main/INFO ]: Unable to start GLFW!\n");
+        return -1;
+    }
+
+    window = glfwCreateWindow(640, 480, "OpenCraft 25r01", NULL, NULL);
+    if (!window)
+    {
+        glfwTerminate();
+        return -1;
+    }
+
+    /* Make the window's context current */
+    glfwMakeContextCurrent(window);
+
+    /* Loop until the user closes the window */
+    while (!glfwWindowShouldClose(window))
+    {
+        /* Render here */
+        // We are going to use a custom library for the rendering because
+        // we can reuse the same code for any OS and any rendering backend.
+        // It could work with OpenGL(ES),DirectX,Vulkan or something like bgfx (C port)
+
+        // ~~~~~ glClear(GL_COLOR_BUFFER_BIT); ~~~~~
+
+        /* Swap front and back buffers */
+        glfwSwapBuffers(window);
+
+        /* Poll for and process events */
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
     return 0;
 }
