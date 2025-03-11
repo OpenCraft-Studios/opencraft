8c96ac990d5c0cfff13ded883a74232da4dc88be
SrFerraaaari <cirod307@gmail.com>
2025-03-11 11:46:29 +0100

    ➖ `cmake/Linux.cmake`: Remove redundant `add_executable`

--- cmake/Linux.cmake
+++ cmake/Linux.cmake
@@ -25,8 +25,5 @@ pkg_check_modules(GLFW REQUIRED glfw3)
 include_directories(${OPENGL_INCLUDE_DIR} ${GLFW_INCLUDE_DIRS})
 link_directories(${GLFW_LIBRARY_DIRS})
 
-# Build executable
-add_executable(${EXECUTABLE_NAME} ${SRC_FILES})
-
 # Apply compilation flags
 target_link_libraries(${EXECUTABLE_NAME} PRIVATE OpenGL::GL ${GLFW_LIBRARIES} ${CMAKE_DL_LIBS} m)
