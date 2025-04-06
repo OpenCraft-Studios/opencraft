aca65afef4faff0ba243e70799c4019a0062e52d
CiroZDP <cirod307@gmail.com>
2025-04-06 18:17:22 +0200

    ➖ Delete MS-DOS

--- cmake/MSDOS.cmake
+++
@@ -1,9 +0,0 @@
-message(STATUS "Building for MS-DOS with DJGPP with Debian")
-
-set(CMAKE_SYSTEM_NAME Generic)
-set(CMAKE_C_COMPILER gcc)
-set(EXECUTABLE_NAME "oc_msdos")
-set(CMAKE_EXECUTABLE_SUFFIX ".exe")
-
-# Build executable
-add_executable(opencraft ${SRC_FILES})
