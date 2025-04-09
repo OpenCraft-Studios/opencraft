bb4efbe15bd531cd3a4d0dd80e78b506ed7f02ec
CiroZDP <cirod307@gmail.com>
2025-04-09 23:18:50 +0200

    ➕ Add base GLAD

--- src/main.c
+++ src/main.c
@@ -1,5 +1,6 @@
 # include <stdio.h>
 # include <GLFW/glfw3.h>
+# include <glad.h>
 
 static GLFWwindow* window;
 
@@ -19,11 +20,13 @@ int main( void )
     /* Make the window's context current */
     glfwMakeContextCurrent(window);
 
+	glClearColor(1, 0, 0, 1);
+
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window))
     {
         /* Render here */
-        // We are gonna implement this later with GLAD
+        glClear(GL_COLOR_BUFFER_BIT);
 
         /* Swap front and back buffers */
         glfwSwapBuffers(window);
