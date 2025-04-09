c436a208f847532e3ef09e894bba636cb502325e
Expungified <161502824+CiroZDP@users.noreply.github.com>
2025-04-09 23:08:31 +0200

    ⛓️ Link glfw (#6)

--- src/main.c
+++ src/main.c
@@ -1,7 +1,38 @@
 # include <stdio.h>
+# include <GLFW/glfw3.h>
 
-int main(void)
+static GLFWwindow* window;
+
+int main( void )
 {
-    printf("Hello world!\n");
+    if (!glfwInit()) {
+        fprintf(stderr, "[ main/ERROR ]: Unable to start GLFW!\n");
+        return -1;
+    }
+
+    window = glfwCreateWindow(640, 480, "OpenCraft 25r01", NULL, NULL);
+    if (!window) {
+        glfwTerminate();
+        return -1;
+    }
+
+    /* Make the window's context current */
+    glfwMakeContextCurrent(window);
+
+    /* Loop until the user closes the window */
+    while (!glfwWindowShouldClose(window))
+    {
+        /* Render here */
+        // We are gonna implement this later with GLAD
+
+        /* Swap front and back buffers */
+        glfwSwapBuffers(window);
+
+        /* Poll for and process events */
+        glfwPollEvents();
+    }
+
+    glfwDestroyWindow(window);
+    glfwTerminate();
     return 0;
 }
