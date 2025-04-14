dd9979a726f6af007bda0e11e3f9052cf2d22716
CiroZDP <cirod307@gmail.com>
2025-04-14 14:23:47 +0200

    🛠️ fix: GLAD not working

--- src/main.c
+++ src/main.c
@@ -2,40 +2,42 @@
 # include <GLFW/glfw3.h>
 # include <glad/glad.h>
 
-static GLFWwindow* window;
+static GLFWwindow *window;
 
-int main( void )
+int main(void)
 {
-    if (!glfwInit()) {
-        fprintf(stderr, "[ main/ERROR ]: Unable to start GLFW!\n");
-        return -1;
-    }
-
     window = glfwCreateWindow(640, 480, "OpenCraft 25r01", NULL, NULL);
-    if (!window) {
-        glfwTerminate();
-        return -1;
-    }
+	if (!glfwInit())
+	{
+		fprintf(stderr, "[ main/ERROR ]: Unable to start GLFW!\n");
+		return -1;
+	}
 
-    /* Make the window's context current */
-    glfwMakeContextCurrent(window);
+	window = glfwCreateWindow(640, 480, "OpenCraft 25r01", NULL, NULL);
+	if (!window)
+	{
+		glfwTerminate();
+		return -1;
+	}
+
+	glfwMakeContextCurrent(window);
+
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+	{
+		fprintf(stderr, "[ main/ERROR ]: Failed to initialize GLAD!\n");
+		return -1;
+	}
 
 	glClearColor(1, 0, 0, 1);
 
-    /* Loop until the user closes the window */
-    while (!glfwWindowShouldClose(window))
-    {
-        /* Render here */
-        glClear(GL_COLOR_BUFFER_BIT);
+	while (!glfwWindowShouldClose(window))
+	{
+		glClear(GL_COLOR_BUFFER_BIT);
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
 
-        /* Swap front and back buffers */
-        glfwSwapBuffers(window);
-
-        /* Poll for and process events */
-        glfwPollEvents();
-    }
-
-    glfwDestroyWindow(window);
-    glfwTerminate();
-    return 0;
+	glfwDestroyWindow(window);
+	glfwTerminate();
+	return 0;
 }
